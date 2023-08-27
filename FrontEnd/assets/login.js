2600cefc51e4465a5162a3c3a2e9741bf79a6274
NicolasM-83200 <manigand.nicolas@gmail.com>
2023-08-27 16:11:02 +0200

    ajout de la page de connexion administrateur fonctionnelle

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,36 @@
+const login = document.querySelector(".login");
+const form = document.querySelector("form");
+
+login.style.fontWeight = "bold";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  //récupération de ce qu'il est renseigné dans les champs "email" et "password"
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  //création d'un objet "user" contenant l'email et le password renseigné
+  let user = {
+    email: email,
+    password: password,
+  };
+
+  //envoie requête a l'api avec la méthode "post"
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      //vérification de la réponse du serveur et redirection en cas d'utilisateur autorisé
+      res.status === 200
+        ? (window.location.href = "/FrontEnd/index.html")
+        : alert("Erreur dans l'identifiant ou le mot de passe");
+      return res.json();
+    })
+    //stockage du token d'authentification dans le session storage
+    .then((data) => sessionStorage.setItem("token", data.token));
+});
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -3,6 +3,15 @@ const gallery = document.querySelector(".gallery");
 let projectData = [];
 let filterData = "all";
 
+// Utilisation d'un Set pour stocker les catégories valides
+const validCategories = new Set([
+  "all",
+  "Objets",
+  "Appartements",
+  "Hotels & restaurants",
+]);
+
+// fonction qui permet la récupération des travaux depuis l'api
 const fetchProjet = async () => {
   await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
@@ -12,17 +21,13 @@ const fetchProjet = async () => {
   projectDisplay();
 };
 
+// fonction d'affichage des travaux avec possibilité de filtrer par catégories
 const projectDisplay = () => {
   gallery.innerHTML = projectData
     .filter((project) => {
-      if (filterData === "all") {
-        return project;
-      } else if (filterData === "object") {
-        return project.categoryId === 1;
-      } else if (filterData === "appart") {
-        return project.categoryId === 2;
-      } else if (filterData === "hostel") {
-        return project.categoryId === 3;
+      if (filterData === "all" || validCategories.has(filterData)) {
+        // Utilisation d'un Set pour vérifier si la catégorie est valide
+        return filterData === "all" || project.category.name === filterData;
       }
     })
     .map(
@@ -36,7 +41,7 @@ const projectDisplay = () => {
     )
     .join("");
 };
-// affichage des élements dans le portfolio
+// affichage des élements dans le portfolio au chargement de la page
 window.addEventListener("load", fetchProjet);
 
 // actions des boutons filtre
@@ -45,14 +50,39 @@ all.addEventListener("click", () => {
   projectDisplay();
 });
 object.addEventListener("click", () => {
-  filterData = "object";
+  filterData = "Objets";
   projectDisplay();
 });
 appart.addEventListener("click", () => {
-  filterData = "appart";
+  filterData = "Appartements";
   projectDisplay();
 });
 hostel.addEventListener("click", () => {
-  filterData = "hostel";
+  filterData = "Hotels & restaurants";
   projectDisplay();
+});
+
+// **************** Administrateur *******************
+const loginBtn = document.querySelector(".login");
+const logoutBtn = document.querySelector(".logout");
+const headerTop = document.querySelector(".header-top");
+const filterBar = document.querySelector(".filter");
+const modIcon = document.querySelector(".mod-icon");
+
+if (sessionStorage.getItem("token")) {
+  loginBtn.style.display = "none";
+  logoutBtn.style.display = "block";
+  headerTop.style.display = "flex";
+  filterBar.style.display = "none";
+  modIcon.style.display = "block";
+}
+
+logoutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sessionStorage.removeItem("token");
+  headerTop.style.display = "none";
+  logoutBtn.style.display = "none";
+  loginBtn.style.display = "block";
+  filterBar.style.display = "flex";
+  modIcon.style.display = "none";
 });
